159f69c706a49967c8ae593e24036b654a80f5a9
https://github.com/Wasi752 <osiur2@gmail.com>
2023-03-25 11:34:33 +0600

    Auth

---
+++ cryptography.js
@@ -0,0 +1,37 @@
+//Checking the crypto module
+const crypto = require('crypto');
+const algorithm = 'aes-256-cbc'; //Using AES encryption
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
+
+//Encrypting text
+function encrypt(text) {
+    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
+    let encrypted = cipher.update(text + ':' + getCurrentDate());
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
+}
+function getCurrentDate() {
+    const today = new Date();
+    return today.getFullYear() + today.getMonth() + today.getDay();
+}
+function check(password, code) {
+    const decrypted = decrypt(code)
+    const [pass, date] = decrypted.split(':')
+    if (pass === password && date === getCurrentDate()) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Decrypting text
+function decrypt(text) {
+    let iv = Buffer.from(text, 'hex');
+    let encryptedText = Buffer.from(text.encryptedData, 'hex');
+    let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
+}
+module.exports = { encrypt, decrypt, check }
---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const { decrypt, encrypt, check } = require('./cryptography')
+const app = express()
+const port = 3000
+const userID = 1
+const userPassword = 'sec123'
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.get('/secret', (req, res) => {
+    res.send('Secret Message!')
+})
+app.get('/secret-2/:id', (req, res) => {
+    const { id } = req.params;
+    if (parseInt(id) === userID) {
+        res.send('Secret Message!')
+    } else {
+        res.send('Unauthorised')
+    }
+})
+app.get('/secret-3', (req, res) => {
+    const { id } = req.query;
+    if (parseInt(id) === userID) {
+        res.send('Secret Message!')
+    } else {
+        res.send('Unauthorised')
+    }
+})
+app.get('/secret-4', (req, res) => {
+    const { id, password } = req.query;
+    if (parseInt(id) === userID && password === userPassword) {
+        res.send('Secret Message!')
+    } else {
+        res.send('Unauthorised')
+    }
+})
+app.get('/secret-5', (req, res) => {
+    const { code } = req.query;
+    if (check(code)) {
+        res.send('Secret Message!')
+    } else {
+        res.send('Unauthorised')
+    }
+})
+app.get('/login', (req, res) => {
+    const { password} = req.query;
+    if (password === userPassword) {
+        res.send(encrypt(password))
+    } else {
+        res.send('Unauthorised')
+    }
+})
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+})
