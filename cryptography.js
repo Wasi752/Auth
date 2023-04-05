11232b0b0cf3b6b6483fe6b83bf4ea387f0b1bdf
wasi752 <osiur2@gmail.com>
2023-04-05 11:09:36 +0600

    Add Authorization

--- cryptography.js
+++ cryptography.js
@@ -5,9 +5,9 @@ const key = crypto.randomBytes(32);
 const iv = crypto.randomBytes(16);
 
 //Encrypting text
-function encrypt(text) {
-    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
-    let encrypted = cipher.update(text + ":" + getCurrentDate());
+function encrypt(id, password) {
+    let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
+    let encrypted = cipher.update(id + ":" + password + ":" + getCurrentDate());
     encrypted = Buffer.concat([encrypted, cipher.final()]);
     return { iv: iv.toString('hex'), code: encrypted.toString('hex') };
 }
@@ -16,24 +16,25 @@ function encrypt(text) {
 function decrypt(text) {
     let iv = Buffer.from(text.iv, 'hex');
     let encryptedText = Buffer.from(text.code, 'hex');
-    let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
+    let decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
     let decrypted = decipher.update(encryptedText);
     decrypted = Buffer.concat([decrypted, decipher.final()]);
     return decrypted.toString();
 }
 function getCurrentDate() {
     const today = new Date();
-    return today.getFullYear() + today.getMonth() + today.getDay();
+    return today.getFullYear() + '.' + today.getMonth() + '.' + today.getDate();
 }
-function check(password, code) {
+function check(user, password, code) {
     try {
         const decrypted = decrypt(code)
-        const [pass, date] = decrypted.split(':')
-        if (pass === password && parseInt(date) === getCurrentDate()) {
-            return true;
-        } else {
-            return false;
-        }
+        const [id, pass, date] = decrypted.split(':')
+        return parseInt(id) === user && pass === password && parseInt(date) === getCurrentDate();
+        // if (parseInt(id) === user && pass === password && parseInt(date) === getCurrentDate()) {
+        //     return true;
+        // } else {
+        //     return false;
+        // }
     } catch (e) {
         return false;
     }
--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const { decrypt, encrypt, check } = require('./cryptography')
 const app = express()
 const cors = require("cors");
 const port = 3000
-const userID = 1
+const userID = 77;
+const officerID = 777;
+const adminID = 7777;
 const userPassword = 'sec123';
+const officerPassword = 'sec1234';
+const adminPassword = 'sec1235';
+
 app.use(cors({
     origin: 'http://localhost:3001',
     credentials: true, // <= Accept credentials (cookies) sent by the client
@@ -49,17 +54,47 @@ app.get('/secret-5', (req, res) => {
     }
 })
 app.get('/secret-6', (req, res) => {
-    const code  = req.header('Authorization');
-    const iv  = req.header('IV');
-    if (check(userPassword, {code, iv})) {
+    const code = req.header('Authorization');
+    const iv = req.header('IV');
+    if (check(userPassword, { code, iv })) {
         res.send('Secret Message!')
     } else {
         res.send('Unauthorised')
     }
 })
-
+app.get('/secret-7', (req, res) => {
+    const code = req.header('Authorization');
+    const iv = req.header('IV');
+    if (check(userID, userPassword, { code, iv })) {
+        res.send('Secret Message!')
+    } else if (check(officerID, userPassword, { code, iv })) {
+        res.send('Welcome!')
+    } else if (check(adminID, userPassword, { code, iv })) {
+        res.send('Most Welcome!')
+    } else {
+        res.send('Unauthorised')
+    }
+})
+app.get('/secret-8', (req, res) => {
+    const code = req.header('Authorization');
+    const iv = req.header('IV');
+    if (check(officerID, userPassword, { code, iv }) || check(adminID, userPassword, { code, iv })) {
+        res.send('Secret Message!')
+    } else {
+        res.send('Unauthorised')
+    }
+})
+app.get('/secret-9', (req, res) => {
+    const code = req.header('Authorization');
+    const iv = req.header('IV');
+    if (check(officerID, officerPassword, { code, iv }) || check(adminID, adminPassword, { code, iv })) {
+        res.send('Secret Message!')
+    } else {
+        res.send('Unauthorised')
+    }
+})
 app.get('/login', (req, res) => {
-    const { password} = req.query;
+    const { password } = req.query;
     if (password === userPassword) {
         res.send(encrypt(password))
     } else {
@@ -67,6 +102,23 @@ app.get('/login', (req, res) => {
     }
 })
 
+app.get('/login-2', (req, res) => {
+    const { id, password } = req.query;
+    if (parseInt(id) === userID && password === userPassword) {
+        res.send(encrypt(password))
+    } else {
+        res.send('Unauthorised')
+    }
+})
+app.get('/login-3', (req, res) => {
+    const { id, password } = req.query;
+    const IDNum = parseInt(id);
+    if ((IDNum === userID || IDNum === officerID || IDNum === adminID) && password === userPassword) {
+        res.send(encrypt(IDNum, password))
+    } else {
+        res.send('Unauthorised')
+    }
+})
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
 })
